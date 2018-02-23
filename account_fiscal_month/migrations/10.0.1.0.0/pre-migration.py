1885ecc33d9905b6833fc94d714bffd0fc213eca
aheficent <ahenriquez@eficent.com>
2018-02-23 17:15:45 +0100

    [IMP]migration scripts for account_fiscal_month

---
+++ account_fiscal_month/migrations/10.0.1.0.0/pre-migration.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# © 2017 Eficent Business and IT Consulting Services S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html)
+from openupgradelib import openupgrade
+
+
+def copy_from_account_period(env):
+    env.cr.execute("""
+        select name, date_start, date_stop
+        from account_period
+        where special = false
+    """)
+    for (name, date_start, date_stop) in env.cr.fetchall():
+        env.cr.execute("select id from date_range where name = '%s'" % name)
+        if not env.cr.fetchone():
+            env.cr.execute("""
+            INSERT into date_range (name, date_start, date_end, type_id)
+            VALUES ('%s', '%s', '%s', 2)        
+            """ % (name,  date_start, date_stop))
+
+
+@openupgrade.migrate(use_env=True)
+def migrate(env, version):
+    copy_from_account_period(env)
