11d61a0d604fb84a4da01574e28a1063e545d1e2
Jordi Ballester Alomar <jordi.ballester@eficent.com>
2019-10-21 10:01:04 +0200

    activate end date as a possible method

--- account_asset_management/models/account_asset_profile.py
+++ account_asset_management/models/account_asset_profile.py
@@ -151,8 +151,7 @@ class AccountAssetProfile(models.Model):
         'Number' and 'End' Time Methods.
         """
         return [
-            ('year', _('Number of Years or end date')),
-        ]
+            ('year', _('Number of Years or end date'))]
 
     @api.multi
     @api.constrains('method')
